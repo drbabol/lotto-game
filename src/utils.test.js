e2be5a89c6eb6cee0d5e3b25a798b65b87686bfa
LG <luca.galeno@gmail.com>
2023-01-25 10:17:11 +0100

    feat(001): refactoring

--- src/utils.test.js
+++ src/utils.test.js
@@ -1,1 +1,12 @@
 const {Bill, howManyBills, initializeBills, printBills}  = require('./utils');
+
+//const prompt = require('prompt-sync')();
+
+
+describe('UnitTests', () => {
+    test('howManyBills() returns 0 when the user enters 0', () => {
+        const howManyBills = jest.fn().mockReturnValue(0);           
+        expect(howManyBills()).toBe();
+    });
+
+});
