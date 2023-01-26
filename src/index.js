8862b7257ffb6c3e4cf22a374bba9a2c44570d77
LG <luca.galeno@gmail.com>
2023-01-26 11:57:37 +0100

    feat(add): added comments

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@
 const {howManyBills, initializeUserBills}  = require('./utils');
 const Bill  = require('./bill');
 
-
+//array of object
 const bills = initializeUserBills(howManyBills())
 
+/**
+ * Function to print my objects bills
+ * @param {Array of Object} bills array of bills
+ * @returns {string} string with nice format
+ */
 const printBills = (bills) => {
-
     if(bills === undefined){return  `Please restart the software and select the number of bills you want to play.`}
 
     const maxBillNChar = 12 
